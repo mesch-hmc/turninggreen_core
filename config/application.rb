2f8e1c4b4db187ad7a5628dd5e801be981e51762
Julia Kei Kawano <jkk2@williams.edu>
2018-08-18 21:32:34 -1000

    front page style changes

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Turninggreen
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.web_console.whitelisted_ips = '172.18.0.1'
   end
 end

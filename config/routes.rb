03b9a84b7c496582ac5a7bb82468cba0a4b85098
Minh To <to.nguyet.minh@gmail.com>
2018-09-14 17:11:01 -0400

    complete challenge pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,18 @@ Rails.application.routes.draw do
   get '/themes' => 'themes#index'
   resources :tips_daily, only: [:index, :show]
 
-  get '/challenges/waste' => 'challenges#index'
+  get '/challenges/waste' => 'challenges#waste'
+  get '/challenges/organic' => 'challenges#organic'
+  get '/challenges/food' => 'challenges#food'
+  get '/challenges/body' => 'challenges#body'
+  get '/challenges/clean' => 'challenges#clean'
+  get '/challenges/home' => 'challenges#home'
+  get '/challenges/water' => 'challenges#water'
+  get '/challenges/biodiversity' => 'challenges#biodiversity'
+  get '/challenges/wellness' => 'challenges#wellness'
+  get '/challenges/fashion' => 'challenges#fashion'
+  get '/challenges/travel' => 'challenges#travel'
+  get '/challenges/energy' => 'challenges#energy'
+  get '/challenges/fair-trade' => 'challenges#fair-trade'
+  get '/challenges/economics' => 'challenges#economics'
 end
